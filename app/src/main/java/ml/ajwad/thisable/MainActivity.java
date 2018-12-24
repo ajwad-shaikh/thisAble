d0a09338bb4206ed6230574bd0c34717f062aa4e
Ajwad <shaikhajwad10@gmail.com>
2018-12-24 16:30:30 +0530

    Main Menu Layout Template Added. Resources Added. Activity FrameWork Added

--- app/src/main/java/ml/ajwad/thisable/MainActivity.java
+++ app/src/main/java/ml/ajwad/thisable/MainActivity.java
@@ -1,7 +1,14 @@
 package ml.ajwad.thisable;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +17,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
---
+++ app/src/main/java/ml/ajwad/thisable/SelectRoute.java
@@ -0,0 +1,13 @@
+package ml.ajwad.thisable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectRoute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_route);
+    }
+}
